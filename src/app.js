c9c5b04cb577fc0b4c472c2e33c2a528fd284c85
ArtemKosiakevych <artem.kosiakevych@gmail.com>
2018-02-21 19:04:37 +0200

    fix: import i18n translations

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
+/* eslint-disable */
 import { AppRegistry } from 'react-native';
-
+import I18n from './I18n/I18n';
 import app from './Movies/MoviesScene'
 AppRegistry.registerComponent('MoviesNote', () => app);
